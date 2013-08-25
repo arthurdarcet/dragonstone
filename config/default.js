213b4e7a794b22d7eec36a9eb9752a618eaec27e
Arthur Darcet <arthur.darcet@m4x.org>
2013-08-25 19:05:00 +0200

    Add a JS button to call an endpoint

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
 	listen: {host: '127.0.0.1', port: 1111},
 	name: 'Example API',
-	base_url: 'http://localhost/v1',
+	base_url: 'http://localhost:1111/test-api',
 	auth: false,
 	endpoints: [{
 		name: 'Users',
